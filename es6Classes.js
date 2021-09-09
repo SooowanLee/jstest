e5030fbfecca2d40d0bbaf5f684458606fc1de82
sooowanlee <lsy9406@gmail.com>
2021-09-09 19:22:53 +0900

    ES6 Classes 중간공부

---
+++ es6Classes.js
@@ -0,0 +1,30 @@
+// ES6 Classes
+
+// const soowan = {
+//   name: 'Soowan',
+//   normal() {
+//     console.log(this.name);
+//   },
+//   arrow: () => {
+//     console.log(this.name);
+//   }
+// }
+
+// soowan.normal();
+// soowan.arrow();
+
+function User(first, last) {
+  this.first = first;
+  this.last = last;
+}
+User.prototype.getFullName = function () {
+  return `${this.first} ${this.last}`;
+}
+
+const soowan = new User('Soowan', 'Lee');
+const hyebin = new User('Hyebin', 'Jeong');
+const dohyun = new User('Dohyun', 'Kim');
+
+console.log(soowan);
+console.log(hyebin.getFullName());
+console.log(dohyun.getFullName());
