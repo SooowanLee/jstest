fdd5c5e9c6b8869982f0ec147eb4e84b427bf2ea
sooowanlee <lsy9406@gmail.com>
2021-09-09 16:35:27 +0900

    callback.js 추가

--- callback.js
+++ callback.js
@@ -1,2 +1,16 @@
 'use strice'
 
+// 콜백(Callback)
+// 함수의 인수로 사용되는 함수
+
+// setTimeeout(함수, 시간)
+
+function timeout(cb) {
+  setTimeout(() => {
+    console.log('Soowan!');
+    cb();
+  }, 3000);
+}
+timeout(() => { // callback함수
+  console.log('Done!');
+});
